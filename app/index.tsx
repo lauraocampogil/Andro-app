ef51f0c254b8d8a25b8f6d451d4e67b6c38ed4fb
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-05-02 12:19:59 +0200

    First page then redirect

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,10 @@
-import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@/lib/auth";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-	const router = useRouter();
-	return (
-		<SafeAreaProvider>
-			<SafeAreaView style={styles.container}>
-				<View style={styles.content}>
-					<Text style={styles.title}>Welcome to ANDRO</Text>
-					<Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-				</View>
-			</SafeAreaView>
-		</SafeAreaProvider>
-	);
-}
+	const { session, initialized } = useAuth();
+	if (!initialized) return null;
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#121212",
-	},
-	content: {
-		flex: 1,
-		paddingHorizontal: 20,
-		paddingTop: 20,
-	},
-	text: {
-		color: "#FFFFFF",
-	},
-	title: {
-		color: "#FFFFFF",
-		fontSize: 24,
-		fontWeight: "bold",
-		marginBottom: 10,
-	},
-});
+	const href = session ? "/(tabs)/home" : "/(auth)/welcome";
+	return <Redirect href={href as any} />;
+}
